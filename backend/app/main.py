043fed597c502490b0e060406548fc03bf8accae
unknown <kamatkartik2006@gmail.com>
2026-03-14 10:57:07 +0530

    .

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
 from app.database.database import Base, engine
+from app.database  import models
 
 app = FastAPI(title="EventFlow API")
 
-# create tables
 Base.metadata.create_all(bind=engine)
 
 
